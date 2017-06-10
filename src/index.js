93b2f0d9d3d3831303e7bdb06eacf553301f36ec
Sarup Banskota <sbanskota08@gmail.com>
2017-06-11 00:32:44 +0700

    Insert name prop within objects discovered

--- src/index.js
+++ src/index.js
@@ -11,3 +11,18 @@
 //                 argument:
 //                   type: "ObjectExpression"
 //                     properties: [Array 1]  ← we wanna inject name here
+
+// Try on console:
+// yarn global add babel-register
+// codemod --require babel-register --plugin src/ test/unnamed_plugins/babel-plugin-syntax-async-functions.js
+export default function({ types: t }) {
+  return {
+    visitor: {
+      ObjectMethod(path) {
+        const nameKey = t.identifier("name");
+        const nameValue = t.stringLiteral("helloworld");
+        path.insertAfter(t.objectProperty(nameKey, nameValue));
+      }
+    }
+  }
+}
